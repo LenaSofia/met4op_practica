d7c1713290caf0a87d53f220b4f34ef49efff572
Lena Sofía <lenasofia51@gmail.com>
2021-09-15 18:00:15 -0300

    Arreglé la función ver_si_es_primo de sacar_numero_primo_nico

--- Práctica/TPS/TP1/sacar_numero_primo_nico.py
+++ Práctica/TPS/TP1/sacar_numero_primo_nico.py
@@ -8,13 +8,14 @@ except:
 
 
 
-def ver_si_es_primo(numero):
-
+def ver_si_es_primo(numero_ingresado):
+    '''Verifica si el número es primo, dividiendolo entre los numeros enteros de 2 a si mismo -1'''
     primo = True # Empezamos asumiendo que el número es primo
 
-    for numero in range(2, es_primo): # Dividimos el número por todos los números entre 2 y el número ingresado menos 1
-        if es_primo % numero == 0: # Si alguna de esas divisiones da como resto 0, definimos al numero como NO primo (primo = False)
+    for numero in range(2, numero_ingresado): # Dividimos el número por todos los números entre 2 y el número ingresado menos 1
+        if numero_ingresado % numero == 0: # Si alguna de esas divisiones da como resto 0, definimos al numero como NO primo (primo = False)
             primo = False
             break # Salimos del ciclo
 
     return primo
+
